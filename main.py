f46a456f25d272802ef06c07d89db62825ce37a1
thiagoverardo <38051267+thiagoverardo@users.noreply.github.com>
2020-11-12 10:50:28 -0300

    terminate_instance+session_resource

--- main.py
+++ main.py
@@ -4,20 +4,49 @@ load_dotenv()
 
 import os
 
+# Importing keys from .env
+
 ACCESS_KEY_ID = os.getenv("ACCESS_KEY_ID")
 SECRET_ACCESS_KEY = os.getenv("SECRET_ACCESS_KEY")
 
 import boto3
 
-ec2 = boto3.client(
-    "ec2",
-    region_name="us-east-1",
+# Initializing session
+
+session = boto3.session.Session(
     aws_access_key_id=ACCESS_KEY_ID,
     aws_secret_access_key=SECRET_ACCESS_KEY,
 )
 
+ec2 = session.resource(
+    "ec2",
+    region_name="us-east-1",
+)
 
-instance = ec2.run_instances(
+# Filtering and terminating running instances
+
+instances_filter = ec2.instances.filter(
+    Filters=[
+        {"Name": "instance-state-name", "Values": ["running"]},
+        {"Name": "tag:Name", "Values": ["VerardoTeste"]},
+    ]
+)
+
+instances_running = []
+for i in instances_filter:
+    instances_running.append(i.id)
+
+if len(instances_running) > 0:
+    for i in instances_filter:
+        ec2.Instance(i.id).terminate()
+        print("Instance {0} terminated".format(i.id))
+
+else:
+    print("No instances to terminate")
+
+# Creating a new instance
+
+instances = ec2.create_instances(
     ImageId="ami-0885b1f6bd170450c",  # ubuntu 20.04 LTS (HVM)
     MinCount=1,
     MaxCount=1,
